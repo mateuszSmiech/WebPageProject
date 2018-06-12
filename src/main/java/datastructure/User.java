10ee06bb4d7a1dda172a36623ce6b52472aea095
matJavaDev <mateusz.smiech@gmail.com>
2018-06-12 23:00:08 +0200

    Configure hibernate

--- src/main/java/datastructure/User.java
+++ src/main/java/datastructure/User.java
@@ -4,10 +4,19 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 @Setter
 @Getter
 @NoArgsConstructor
 public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+
     private String firstName;
     private String lastName;
     private String username;
